6d70efa04b45ddb52c3e58e37341570a798fc0d1
Jonathan Corea <45439228+jacorea@users.noreply.github.com>
2021-04-26 09:45:21 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import "./styles.css";
+
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+export default function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
+}

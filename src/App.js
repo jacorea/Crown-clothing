3c0e15c21efda149bed87959a2a4f023f999d334
Jonathan Corea <jacorea410@gmail.com>
2021-04-27 11:45:00 -0700

    add sign-in component and sign-in component button and FormInput.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import "./styles.css";
 
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 export default function App() {
   return (
@@ -18,6 +14,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
